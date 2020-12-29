84dfddb3d7bb3fcd8f4148ab044bd778d462d6b4
Arvin Ken M Ramirez <arvin.ken.ramirez@ulaps.dev>
2020-12-29 14:51:14 +0800

    updated more info section and alert on submit btn

---
+++ js/ownJS.js
@@ -0,0 +1,17 @@
+function sendMessage(){
+    alert("Hi, Your Message Has Been Sent. Thank You For Choosing Us");
+    
+}
+// function getName(){
+//     var got_name = document.getElementById("inpname").value;
+//     return got_name;
+// }
+// function getEmail(){
+//     var got_email = document.getElementById("inpemail").value;
+// }
+// function getMessage(){
+//     var got_message = document.getElementById("inpmessage").value;
+// }
+// function sendMail(){
+
+// }
